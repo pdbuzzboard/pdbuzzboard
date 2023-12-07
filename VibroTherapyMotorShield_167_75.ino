b405e99e275403f3c43bc9078870234d59927d94
pdbuzzboard <126915663+pdbuzzboard@users.noreply.github.com>
2023-12-07 10:20:46 -0500

    Add files via upload
    Based upon requests for a Sketch that uses the 167 millisecond timing, but has a lower vibration strength, I have created two additional Sketches.  The 167_75 vibrates the motors at 75% of the intensity of the standard _167.  The 167_90 vibrates at 90% of the standard.

---
+++ VibroTherapyMotorShield_167_75.ino
@@ -0,0 +1,75 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+// updated for PD Vibrotherapy stimulation pattern
+// 2023-01-25
+
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+int timer = 167;           // The higher the number, the slower the timing.
+int ledPins[] = { 3, 2, 4, 1, 1, 2, 4, 3, 2, 4, 3, 1, 13, 13, 13, 13, 13, 13, 13, 13, 2, 4, 3, 1, 2, 1, 3, 4, 1, 3, 4, 2 };
+// an array of pin numbers to which LEDs are attached Pin 13 is a pause
+int pinCount = 32;           // the number of pins (i.e. the length of the array)
+
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+  pinMode(13, OUTPUT);
+
+  // turn on motor
+  motor1.setSpeed(191);
+ 
+  motor1.run(RELEASE);
+  motor2.setSpeed(191);
+ 
+  motor2.run(RELEASE);
+  motor3.setSpeed(191);
+ 
+  motor3.run(RELEASE);
+  motor4.setSpeed(191);
+ 
+  motor4.run(RELEASE);
+}
+
+void loop() {
+for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+
+  switch (ledPins[thisPin]) { 
+    case 1:
+     motor1.run(FORWARD);
+      motor1.setSpeed(191);  
+      delay(timer);
+      motor1.run(RELEASE);
+      break;
+    case 2:
+      motor2.run(FORWARD);
+      motor2.setSpeed(191);  
+      delay(timer);
+      motor2.run(RELEASE);
+      break;
+    case 3:
+      motor3.run(FORWARD);
+      motor3.setSpeed(191);  
+      delay(timer);
+      motor3.run(RELEASE);
+      break;
+    case 4:
+      motor4.run(FORWARD);
+      motor4.setSpeed(191);  
+      delay(timer);
+      motor4.run(RELEASE);
+      break;
+    case 13:
+      digitalWrite(13, HIGH);
+      delay(timer);
+      digitalWrite(13, LOW);
+      break;
+  }
+}
+}
